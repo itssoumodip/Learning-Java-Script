df19926f0d76c103f2c74f1354103bcdb92f9d18
Soumodip Das <138969047+itssoumodip@users.noreply.github.com>
2024-04-02 15:49:27 +0000

    add all the files

---
+++ video 57/index.js
@@ -0,0 +1,37 @@
+console.log("HELLOW THIS IS LOOKING LIKE A WOW !!")
+
+// // LOOP ->
+// let a = 1;
+
+// // NORMAL FOR LOOP 
+// for (let i = 0; i < 100; i++) {
+//     console.log(i)    
+// }
+// //FOR IN
+// let obj = {
+//     name: "Soumodip",
+//     role: "Programmer", 
+//     company: "Soumodip AI"
+// }
+// // FOR OF 
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key, element)
+// }
+// // FOR OF
+// for (const c of "Soumodip") {
+//     console.log(c)
+// }
+// // WHILE
+// let i=0;
+// while(i<=6){
+//     console.log(i)
+//     i++;
+// }
+//
+// //DO WHILE LOOP
+let i=0;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
