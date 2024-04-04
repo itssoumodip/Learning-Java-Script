228f512eb07b94e8b24f1693f488ba25a37d3aa7
Soumodip Das <138969047+itssoumodip@users.noreply.github.com>
2024-04-04 05:33:21 +0000

    adding codes

--- video 61/index.js
+++ video 61/index.js
@@ -25,8 +25,9 @@ let obj = {
 }
 
 if (random > 0.1) {
-
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
 }
 else {
-    
+    c = obj[c];
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
 }
